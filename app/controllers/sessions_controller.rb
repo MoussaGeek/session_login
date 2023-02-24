d0f664c4859f616b84405b692eb69c6bb00bb291
MoussaGeek <moussadiakite1963@gmail.com>
2023-02-24 11:09:36 +0000

    session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,23 @@
 class SessionsController < ApplicationController
+    skip_before_action :login_required, only: [:new, :create]
     
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            log_in(user)
+            redirect_to user_path(user.id)
+        else
+            flash.now[:danger] = 'échec de la connexion'
+            render :new
+        end
+      end
+      def destroy
+        session.delete(:user_id)
+        flash[:notice] = 'déconnecté'
+    redirect_to new_session_path
+      end
+    # 省略
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+    before_action :correct_user, only: [:show]
+    skip_before_action :login_required, only: [:new, :create]
 
     def new
       @user = User.new
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        log_in(@user)
         redirect_to user_path(@user.id)
       else
         render :new
@@ -20,6 +22,11 @@ class UsersController < ApplicationController
     
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to current_user unless current_user?(@user)
     end
     
 end
